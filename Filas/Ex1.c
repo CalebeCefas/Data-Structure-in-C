009619cdc02a7c0795f6bb661c2087d02da40856
Cefasss <program.cefas@gmail.com>
2025-01-27 16:33:10 -0300

    Atualizações

--- Filas/Ex1.c
+++ Filas/Ex1.c
@@ -4,77 +4,88 @@
 #define TAMANHO 5
 
 typedef struct {
-    int itens[TAMANHO], inicio, fim;
+    int itens[TAMANHO], prioridade[TAMANHO], inicio, fim;
 } Fila;
 
-void inicializarFila (Fila *f) {
+void inicializarFila(Fila *f) {
     f->inicio = -1;
     f->fim = -1;
 }
 
-int estaVazia (Fila *f) {
+int estaVazia(Fila *f) {
     return (f->inicio == -1);
 }
 
-int estaCheia (Fila *f) {
-    return (f->fim == TAMANHO -1);
+int estaCheia(Fila *f) {
+    return (f->fim == TAMANHO - 1);
 }
 
-void enfileirar (Fila *f, int valor) {
-    if(estaCheia(f)) {
-        printf("ERRO: Fila cheia");
+void enfileirar(Fila *f, int valor, int prioridade) {
+    if (estaCheia(f)) {
+        printf("ERRO: Fila cheia\n");
+        return;
     } else if (estaVazia(f)) {
         f->inicio = 0;
     }
 
     f->fim++;
-    f->itens[f->fim] = valor;
-    printf("Elemento %d foi enfileirado!\n", valor);
+    int i;
+    for (i = f->fim - 1; i >= f->inicio && f->prioridade[i] < prioridade; i--) {
+        f->itens[i + 1] = f->itens[i];
+        f->prioridade[i + 1] = f->prioridade[i];
+    }
+
+    f->itens[i + 1] = valor;
+    f->prioridade[i + 1] = prioridade;
+
+    printf("Elemento %d com prioridade %d foi enfileirado!\n", valor, prioridade);
 }
 
-int desenfileirar(Fila *f){
+int desenfileirar(Fila *f) {
     if (estaVazia(f)) {
-        printf("ERRO: Fila vazia");
+        printf("ERRO: Fila vazia\n");
         return -1;
     }
 
     int valorRemovido = f->itens[f->inicio];
 
     if (f->inicio >= f->fim) {
-        //Resetar fila
+        // Resetar fila
         inicializarFila(f);
-        printf("Fila reinicializada");
     } else {
-        f-> inicio++;
+        f->inicio++;
     }
 
     return valorRemovido;
 }
 
-void exibirFila (Fila *f) {
+void exibirFila(Fila *f) {
     if (estaVazia(f)) {
-        printf("Fila vazia");
+        printf("Fila vazia\n");
         return;
     }
 
-    printf("Elementos da fila: ");
-    for(int i = f->inicio; i <= f->fim; i++){
-        printf("%d ", f->itens[i]);
+    printf("\nElementos da fila (valor, prioridade): ");
+    for (int i = f->inicio; i <= f->fim; i++) {
+        printf("(%d, %d) ", f->itens[i], f->prioridade[i]);
     }
+    printf("\n");
 }
 
-int main () {
+int main() {
     Fila fila;
 
     inicializarFila(&fila);
 
-    enfileirar(&fila, 10);
-    enfileirar(&fila, 20);
-    enfileirar(&fila, 40);
+    enfileirar(&fila, 10, 2);
+    enfileirar(&fila, 20, 1);
+    enfileirar(&fila, 40, 3);
 
     exibirFila(&fila);
 
     desenfileirar(&fila);
 
     exibirFila(&fila);
+
+    return 0;
 }
